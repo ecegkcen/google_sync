2575fc0ff9964d499e9d9824862531df8b857e30
ecegkcen <ecegokcen.sari@std.yeditepe.edu.tr>
2025-11-29 02:49:28 +0300

    Fix ProductsControllerTest: use correct path helpers

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,22 +2,24 @@ require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get products_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get products_show_url
+    get products_path
     assert_response :success
   end
 
   test "should get new" do
-    get products_new_url
+    get new_product_path
+    assert_response :success
+  end
+
+  test "should get show" do
+    product = Product.create(name: "Test Ürün", price: 10.0, stock: 5, category: "Deneme")
+    get product_path(product)
     assert_response :success
   end
 
   test "should get edit" do
-    get products_edit_url
+    product = Product.create(name: "Test Ürün", price: 10.0, stock: 5, category: "Deneme")
+    get edit_product_path(product)
     assert_response :success
   end
 end
